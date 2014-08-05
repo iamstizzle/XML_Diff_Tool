0d95a185939b8cadfd3714dacb86c17d866e4236
iamstizzle <thatguyjoshua@yahoo.com>
2014-08-05 13:38:55 -0500

    Xml updates
    Some minor tweaks

--- XML_diff2.py
+++ XML_diff2.py
@@ -5,9 +5,8 @@ import xml.etree.ElementTree as ET
 
 #import your xml here. xlmdoc === values for new
 #xmldoc_old === values for old to remove same.
-xmldoc = ET.parse("testfailures.xml")
-xmldoc_old = ET.parse("testfailures1277.xml")
-
+xmldoc = ET.parse("testfailures_m10.xml")
+xmldoc_old = ET.parse("testfailures_m9.xml")
 
 
 
@@ -17,11 +16,10 @@ rootold = xmldoc_old.getroot()
 
 xmldoc.write('output.xml')
 
-#print (root)
 
 
 
-### Math logic check ##############################################################
+### Math logic check not necessary at all                ##########################
 ## Finds failures in each .xml file and puts them into an array for stat tallying##
 ###################################################################################
 
@@ -65,13 +63,17 @@ totalstats = str( ("ORIGINAL FAILURES: ", len(failurearray), " LESS DUPLICATES",
 
 ### end logic checks #######
 #####################################################################################
-
+##http://stackoverflow.com/questions/1867861/python-dictionary-keep-keys-values-in-same-order-as-declared
 
 
 #### MODIFY XML FILE #########
 ### TURNS DUPLICATE FAILURES INTO MESSAGES - as messages are not generated ###
 ### use test.remove() if removal of failures becomes necessary ############
 ### https://docs.python.org/2/library/xml.etree.elementtree.html
+### http://stackoverflow.com/questions/2741480/can-elementtree-be-told-to-preserve-the-order-of-attributes
+### http://stackoverflow.com/questions/13225254/preserving-python-dictionary-order-continuation-of-python-dict
+#http://stackoverflow.com/questions/14917943/python-lxml-enforcing-an-specific-order-for-attributes
+########33
 
 for test in root.iter('test'):
     count = 0
@@ -82,23 +84,24 @@ for test in root.iter('test'):
             if testmatch.attrib["result"] =="F":
                 if test.attrib["id"] == testmatch.attrib["id"]:
                     count = 1
-                    test.attrib["result"] = "MATCHREMOVALASFAILURE"
+                    test.attrib["result"] = "MATCHED_FAILURE"
+      
     if count == 0:
         nomatch = True
         #print ("NoMatchFound")  ## might print a ton for every non Failure in the root.iter('test") that is not a failure
         
 
-xmldoc.write('output2.xml')
+xmldoc.write('testfailures.xml')
 
 count = 0
 for each in root.iter("test"):
-    if each.attrib["result"] == "MATCHREMOVALASFAILURE":
+    if each.attrib["result"] == "MATCHED_FAILURE":
         count +=1
 print (count, "matches found \n")
     
 count = 0
 
-### print out summary file ###
+### print out summary file. Math logic check necessary for stat ouput###
 f = open("stats.txt", 'w')
 f.write( "Failures in file 1: %d" % (len(failurearray)))
 f.write( "\nFailures in file 2: %d" % (len(failurearrayold)))
